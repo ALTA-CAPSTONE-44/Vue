c006ba6f3c33860b76757860a8d2eeb295a4a03e
Oriel Zulfaliani <orielzulfa@gmail.com>
2022-07-04 18:22:41 +0700

    nambah/revisi stock

--- pages/admin/Stock.vue
+++ pages/admin/Stock.vue
@@ -2,11 +2,41 @@
   <v-container>
     <CardStockVaksin />
 
-    <div style="margin-top: 40px" class="grid-vact">
+    <!-- <div style="margin-top: 40px" class="grid-vact">
       <CardStockVaksin2 />
       <CardStockVaksin2 />
       <CardStockVaksin2 />
       <CardStockVaksin2 />
+    </div> -->
+
+    <div>
+      <div class="text-xl my-6">Laporan pengambilan vaksin</div>
+      <table>
+        <tr>
+          <th style="border-radius: 8px">No.</th>
+          <th>Tanggal</th>
+          <th>Nama Vaksin</th>
+          <th style="border-radius: 8px">Jumlah</th>
+        </tr>
+        <tr>
+          <td class="td" style="border-radius: 8px">10001</td>
+          <td>Tom</td>
+          <td>M</td>
+          <td style="border-radius: 8px">30</td>
+        </tr>
+        <tr>
+          <td class="td" style="border-radius: 8px">10002</td>
+          <td>Sally</td>
+          <td>F</td>
+          <td style="border-radius: 8px">28</td>
+        </tr>
+        <tr>
+          <td class="td" style="border-radius: 8px">10003</td>
+          <td>Emma</td>
+          <td>F</td>
+          <td style="border-radius: 8px">24</td>
+        </tr>
+      </table>
     </div>
   </v-container>
 </template>
@@ -22,5 +52,26 @@ export default {
   display: grid;
   grid-template-columns: 420px 420px;
   gap: 10px;
+}
+table {
+  width: 90%;
+  border-collapse: separate;
+  border-spacing: 0 15px;
+}
+tr {
+  background-color: #fff;
+}
+th {
+  background-color: #fff;
+  color: #06152b;
+}
+th,
+td {
+  width: 150px;
+  text-align: center;
+  padding: 10px;
+}
+h2 {
+  color: #4287f5;
 }
 </style>
---
+++ pages/admin/StockMake.vue
@@ -0,0 +1,188 @@
+<template>
+  <v-container>
+    <section class="d-flex">
+      <div class="mr-14">
+        <div>Pilih Sesi</div>
+
+        <div class="mt-8">
+          <div class="gap">
+            <label class="custom-checkbox mr-3">
+              <input type="hidden" name="alarm" value="False" />
+              <input
+                class="custom-checkbox-input"
+                name="alarm"
+                value="True"
+                type="checkbox"
+              />
+              <span class="custom-checkbox-text">Sesi 1</span>
+            </label>
+            <label class="custom-checkbox">
+              <input type="hidden" name="alarm" value="False" />
+              <input
+                class="custom-checkbox-input"
+                name="alarm"
+                value="True"
+                type="checkbox"
+              />
+              <span class="custom-checkbox-text">Sesi 2</span>
+            </label>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div>Waktu</div>
+
+        <div class="mt-8">
+          <div class="gap">
+            <label class="custom-checkbox mr-3">
+              <input type="hidden" name="alarm" value="False" />
+              <input
+                class="custom-checkbox-input"
+                name="alarm"
+                value="True"
+                type="checkbox"
+              />
+              <span class="custom-checkbox-text">Sesi 1</span>
+            </label>
+            <label class="custom-checkbox">
+              <input type="hidden" name="alarm" value="False" />
+              <input
+                class="custom-checkbox-input"
+                name="alarm"
+                value="True"
+                type="checkbox"
+              />
+              <span class="custom-checkbox-text">Sesi 2</span>
+            </label>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <div class="mt-10">
+        <div>Pembukaan Pendaftaran</div>
+        <v-row>
+          <v-col cols="12" sm="6" md="4">
+            <v-text-field label="Solo" class="py-lg-3" solo></v-text-field>
+          </v-col>
+
+          <v-col cols="12" sm="6" md="4">
+            <v-text-field label="Solo" class="py-lg-3" solo></v-text-field>
+          </v-col>
+        </v-row>
+      </div>
+    </section>
+
+    <section class="d-flex">
+      <div class="mr-14">
+        <div>Jenis Vaksin</div>
+
+        <div class="mt-8">
+          <div class="gap">
+            <label class="custom-checkbox mr-3">
+              <input type="hidden" name="alarm" value="False" />
+              <input
+                class="custom-checkbox-input"
+                name="alarm"
+                value="True"
+                type="checkbox"
+              />
+              <span class="custom-checkbox-text">Vaksin Sinovac</span>
+            </label>
+            <label class="custom-checkbox mr-3">
+              <input type="hidden" name="alarm" value="False" />
+              <input
+                class="custom-checkbox-input"
+                name="alarm"
+                value="True"
+                type="checkbox"
+              />
+              <span class="custom-checkbox-text">Vaksin Astrazeneca</span>
+            </label>
+            <label class="custom-checkbox mr-3">
+              <input type="hidden" name="alarm" value="False" />
+              <input
+                class="custom-checkbox-input"
+                name="alarm"
+                value="True"
+                type="checkbox"
+              />
+              <span class="custom-checkbox-text">Vaksin Moderna</span>
+            </label>
+            <label class="custom-checkbox">
+              <input type="hidden" name="alarm" value="False" />
+              <input
+                class="custom-checkbox-input"
+                name="alarm"
+                value="True"
+                type="checkbox"
+              />
+              <span class="custom-checkbox-text">Vaksin Pfizer</span>
+            </label>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="mt-11">
+      <div class="mr-14">
+        <div>Stock Vaksin</div>
+
+        <div>
+          <v-row cols="3">
+            <v-col>
+              <v-text-field
+                label="Isi Stock"
+                placeholder="Placeholder"
+                solo
+              ></v-text-field>
+            </v-col>
+            <v-col cols="3">
+              <div class="text-sm">Stock Vaksin Tersedia</div>
+              <div class="text-xl">Sinovac 240</div>
+            </v-col>
+            <v-col cols="6"></v-col>
+          </v-row>
+        </div>
+
+        <div class="my-2">
+          <v-btn x-large style="background-color: #0fe0cb" dark>
+            Tambahkan Sesi
+          </v-btn>
+        </div>
+      </div>
+    </section>
+  </v-container>
+</template>
+
+<script>
+export default {
+  layout: "adminLayout",
+};
+</script>
+
+<style >
+.input-cus {
+  background-color: #fff;
+}
+.custom-checkbox-input {
+  display: none;
+  margin-top: 30px;
+}
+
+.custom-checkbox-text {
+  border-radius: 10px;
+  padding: 20px 40px;
+  background-color: white;
+  color: #555;
+  cursor: pointer;
+  user-select: none;
+}
+
+.custom-checkbox-input:checked ~ .custom-checkbox-text {
+  background-color: #0fe0cb;
+  color: white;
+}
+</style>
