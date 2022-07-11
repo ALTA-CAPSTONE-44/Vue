448581ce89d64d7befe4ef036f0631c023e3ca8a
Wanda Alifah <wanda.alifah@students.unnes.ac.id>
2022-07-11 12:15:14 +0700

    edit setting views

--- components/core/ManageUser.vue
+++ components/core/ManageUser.vue
@@ -3,7 +3,7 @@
         <v-card class="mt-8 pa-4">
             <v-row>
                 <v-col class="d-flex justify-start">
-                    <h3 class="mt-2">Account Admin</h3>
+                    <h3 class="mt-2">Account User</h3>
                     <v-spacer></v-spacer>
                     <v-text-field
                     prepend-inner-icon="mdi-magnify"
@@ -15,10 +15,6 @@
                     width="250px"
                     class="shrink mx-6"
                     ></v-text-field>
-                    <v-btn
-                    style="color: white"
-                    color="#45C6FC"><v-icon>mdi-plus</v-icon>
-                    Tambah Admin</v-btn>
                 </v-col>
             </v-row>
             <v-divider></v-divider>
@@ -29,13 +25,16 @@
                     color="#44444"></v-checkbox>
                 </v-col>
                 <v-col cols="12" md="2" class="mt-5">
-                   <p>Status</p> 
+                   <p>Akun Dibuat</p> 
                 </v-col>
-                <v-col cols="12" md="4" class="mt-5">
-                    <p>Tempat Kesehatan</p>
+                <v-col cols="12" md="2" class="mt-5">
+                    <p>Email</p>
+                </v-col>
+                <v-col cols="12" md="2" class="mt-5">
+                    <p>NIK</p>
                 </v-col>
                 <v-col cols="12" md="3" class="mt-5">
-                    <p>Team</p>
+                    <p>Kontak Tertaut</p>
                 </v-col>
             </v-row>
         </v-card>
@@ -43,43 +42,23 @@
             <v-row class="d-flex justify-start">
                 <v-col cols="12" md="3">
                     <v-checkbox
-                    label="Ronald Richard"
+                    label="Ahmad Zaid"
                     color="#44444"></v-checkbox>
                 </v-col>
                 <v-col cols="12" md="2" class="mt-5">
-                   <v-btn color="success">Aktif</v-btn> 
-                </v-col>
-                <v-col cols="12" md="4" class="mt-5">
-                    <p>Rumah Sakit Sultan Haldirin</p>
+                   <p>01/01/2022</p> 
                 </v-col>
                 <v-col cols="12" md="2" class="mt-5">
-                    <p>Admin Nakes</p>
+                    <p>name@gmail.com</p>
                 </v-col>
-                <v-col cols="12" md="1" class="d-flex">
+                <v-col cols="12" md="2" class="mt-5">
+                    <p>3375920894810001</p>
+                </v-col>
+                <v-col cols="12" md="2" class="mt-5">
+                    <p>Kontak Tertaut</p>
+                </v-col>
+                <v-col cols="12" md="1" class="mt-5">
                     <v-icon large>mdi-delete-outline</v-icon>
-                    <v-icon large>mdi-pencil-outline</v-icon>
-                </v-col>
-            </v-row>
-        </v-card>
-        <v-card class="mt-3 pa-4">
-            <v-row class="d-flex justify-start">
-                <v-col cols="12" md="3">
-                    <v-checkbox
-                    label="Ester Howard"
-                    color="#44444"></v-checkbox>
-                </v-col>
-                <v-col cols="12" md="2" class="mt-5">
-                   <v-btn color="success">Aktif</v-btn> 
-                </v-col>
-                <v-col cols="12" md="4" class="mt-5">
-                    <p>Rumah Sakit Sultan Haldirin</p>
-                </v-col>
-                <v-col cols="12" md="2" class="mt-5">
-                    <p>Admin Nakes</p>
-                </v-col>
-                <v-col cols="12" md="1" class="d-flex">
-                    <v-icon large>mdi-delete-outline</v-icon>
-                    <v-icon large>mdi-pencil-outline</v-icon>
                 </v-col>
             </v-row>
         </v-card>
--- pages/admin/setting.vue
+++ pages/admin/setting.vue
@@ -6,7 +6,8 @@
             <h3 class="mr-6">Manage Account User</h3>
         </v-row>
         <!-- <core-setting-account /> -->
-        <core-manage-admin />
+        <!-- <core-manage-admin /> -->
+        <core-manage-user />
     </v-container>
 </template>
 
@@ -25,6 +26,7 @@ import AppBar from '~/components/core/AppBar.vue'
 import Vaksinbooking from '../vaksinbooking.vue'
 import SettingAccount from '@/components/core/SettingAccount.vue'
 import ManageAdmin from '~/components/core/ManageAdmin.vue'
+import ManageUser from '~/components/core/ManageUser.vue'
 export default {
   name: 'SettingPage',
   layout: 'adminLayout',
@@ -36,6 +38,7 @@ export default {
     Vaksinbooking,
     CoreSettingAccount: SettingAccount,
     CoreManageAdmin: ManageAdmin,
+    CoreManageUser: ManageUser,
   },
 }
 </script>
